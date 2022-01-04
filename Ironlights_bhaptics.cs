f8376a4283c6cd891f419a4d2531105376e22004
floh-bhaptics <floh@fahrenbergers.de>
2022-01-04 22:05:46 +0100

    Dashing and Damage

--- Ironlights_bhaptics.cs
+++ Ironlights_bhaptics.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using MelonLoader;
 using HarmonyLib;
+using UnityEngine;
 
 using MyBhapticsTactsuit;
 
@@ -22,13 +23,94 @@ namespace Ironlights_bhaptics
             tactsuitVr.PlaybackHaptics("HeartBeat");
         }
 
-        [HarmonyPatch(typeof(Fighter), "Retreat")]
+        [HarmonyPatch(typeof(Fighter), "Retreat", new Type[] { })]
         public class bhaptics_Retreat
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Fighter __instance)
+            {
+                if (!__instance.isHost) return;
+                tactsuitVr.PlaybackHaptics("Retreat");
+            }
+        }
+
+        [HarmonyPatch(typeof(Fighter), "Die", new Type[] { })]
+        public class bhaptics_Die
         {
             [HarmonyPostfix]
             public static void Postfix()
             {
-                tactsuitVr.PlaybackHaptics("HeartBeat");
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(Fighter), "Blitz", new Type[] { })]
+        public class bhaptics_Blitz
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Fighter __instance)
+            {
+                if (!__instance.isHost) return;
+                tactsuitVr.PlaybackHaptics("Blitz");
+            }
+        }
+
+        private static KeyValuePair<float, float> getAngleAndShift(Transform player, Vector3 hit)
+        {
+            // bhaptics pattern starts in the front, then rotates to the left. 0° is front, 90° is left, 270° is right.
+            // y is "up", z is "forward" in local coordinates
+            Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+            Vector3 hitPosition = hit - player.position;
+            Quaternion myPlayerRotation = player.rotation;
+            Vector3 playerDir = myPlayerRotation.eulerAngles;
+            // get rid of the up/down component to analyze xz-rotation
+            Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+
+            // get angle. .Net < 4.0 does not have a "SignedAngle" function...
+            float hitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+            // check if cross product points up or down, to make signed angle myself
+            Vector3 crossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+            if (crossProduct.y > 0f) { hitAngle *= -1f; }
+            // relative to player direction
+            float myRotation = hitAngle - playerDir.y;
+            // switch directions (bhaptics angles are in mathematically negative direction)
+            myRotation *= -1f;
+            // convert signed angle into [0, 360] rotation
+            if (myRotation < 0f) { myRotation = 360f + myRotation; }
+
+
+            // up/down shift is in y-direction
+            // in Shadow Legend, the torso Transform has y=0 at the neck,
+            // and the torso ends at roughly -0.5 (that's in meters)
+            // so cap the shift to [-0.5, 0]...
+            float hitShift = hitPosition.y;
+            float upperBound = 0.0f;
+            float lowerBound = -0.5f;
+            if (hitShift > upperBound) { hitShift = 0.5f; }
+            else if (hitShift < lowerBound) { hitShift = -0.5f; }
+            // ...and then spread/shift it to [-0.5, 0.5]
+            else { hitShift = (hitShift - lowerBound) / (upperBound - lowerBound) - 0.5f; }
+
+            //tactsuitVr.LOG("Relative x-z-position: " + relativeHitDir.x.ToString() + " "  + relativeHitDir.z.ToString());
+            //tactsuitVr.LOG("HitAngle: " + hitAngle.ToString());
+            //tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+
+            // No tuple returns available in .NET < 4.0, so this is the easiest quickfix
+            return new KeyValuePair<float, float>(myRotation, hitShift);
+        }
+
+
+        [HarmonyPatch(typeof(Fighter), "TakeHit", new Type[] { typeof(float), typeof(Vector3), typeof(Vector3), typeof(bool), typeof(bool) })]
+        public class bhaptics_TakeHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Fighter __instance, float damage, Vector3 point, Vector3 normal, bool extraDamage, bool doublehit)
+            {
+                if (!__instance.isHost) return;
+                if (__instance.health <= 0.25f * __instance.maxHealth) tactsuitVr.StartHeartBeat();
+                Transform myPlayer = __instance.transform;
+                var angleShift = getAngleAndShift(myPlayer, point);
+                tactsuitVr.PlayBackHit("Impact", angleShift.Key, angleShift.Value);
             }
         }
 
