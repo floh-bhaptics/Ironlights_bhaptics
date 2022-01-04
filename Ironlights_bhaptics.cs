cb262e6a077d349c91a39c0d8cfd9f9f604bc783
floh-bhaptics <floh@fahrenbergers.de>
2022-01-04 17:50:48 +0100

    Projektdateien hinzufügen.

---
+++ Ironlights_bhaptics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+
+using MyBhapticsTactsuit;
+
+namespace Ironlights_bhaptics
+{
+    public class Ironlights_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(Fighter), "Retreat")]
+        public class bhaptics_Retreat
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("HeartBeat");
+            }
+        }
+
+    }
+}
